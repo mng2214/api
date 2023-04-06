5297ec14d84c030c53dd044a8103f17d4bbad33d
mng2214 <108999655+mng2214@users.noreply.github.com>
2023-04-05 20:59:54 -0500

    a

--- src/test/java/get/StarWars.java
+++ src/test/java/get/StarWars.java
@@ -1,4 +1,25 @@
 package get;
 
+import io.restassured.RestAssured;
+import org.junit.Test;
+
 public class StarWars {
+
+    /*
+    1 defined/determined the endpoint
+    2 added query string params as needed
+    3 defined HTTP method
+    4 send
+    validate status code
+     */
+
+    @Test
+    public void getSWChars() {
+        //https://swapi.dev/api/people
+        RestAssured.given()
+                .when()
+                .get("https://swapi.dev/api/people")
+                .then().statusCode(200)
+                .log().body();
+    }
 }
